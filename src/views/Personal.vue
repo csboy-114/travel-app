7bc86b5fb4b50a78293b4c02972d345ebb3d8705
csboy-114 <1060437021@qq.com>
2021-11-28 19:10:19 +0800

    完成我的页面

--- src/views/Personal.vue
+++ src/views/Personal.vue
@@ -1,19 +1,102 @@
 <template>
   <div class="personal">
-    <h1>personal</h1>
-    <tabbar/>
+    <div class="top-bg"></div>
+    <div class="top-avatar">
+      <img src="../assets/images/User_base.png" alt="" />
+      <h1>王豆豆</h1>
+      <p>18911023266</p>
+    </div>
+    <div class="list">
+      <div class="list-item">
+        <p>我的行程计划</p>
+        <i class="iconfont icon-right"></i>
+      </div>
+       <div class="list-item">
+        <p>我的收藏</p>
+        <i class="iconfont icon-right"></i>
+      </div>
+       <div class="list-item">
+        <p>售后于客服</p>
+        <i class="iconfont icon-right"></i>
+      </div>
+       <div class="list-item">
+        <p>我的优惠券</p>
+        <i class="iconfont icon-right"></i>
+      </div>
+       <div class="list-item">
+        <p>常用旅客管理</p>
+        <i class="iconfont icon-right"></i>
+      </div>
+    </div>
+    <tabbar />
   </div>
 </template>
 
 <script>
-import tabbar from '../components/tabbar'
+import tabbar from "../components/tabbar";
 export default {
-  name:'Personal',
-  components:{
-    tabbar
+  name: "Personal",
+  components: {
+    tabbar,
   },
-  created(){
-    console.log('personal created');
+  created() {},
+};
+</script>
+
+<style lang="scss">
+.personal {
+  width: 100vw;
+  height: 100vh;
+  background-color: #fff;
+  .top-bg {
+    width: 100vw;
+    height: 149px;
+    background-image: url("../assets/images/Cover.png");
+  }
+  .top-avatar{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: -60px;
+    img{
+      width: 139px;
+      height: 139px;
+    }
+    h1{
+      margin-bottom: 6px;
+      font-family: PingFangSC-Regular;
+      font-size: 20px;
+      color: #203152;
+      letter-spacing: 1px;
+    }
+    p{
+      font-family: PingFangSC-Regular;
+      font-size: 15px;
+      color: #7C8698;
+      letter-spacing: 1px;
+    }
+  }
+  .list{
+    margin-top: 52px;
+    &-item{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 20px;
+      margin-bottom: 20px;
+      p{
+        font-family: PingFangSC-Regular;
+        font-size: 16px;
+        font-weight: 500;
+        color: #262626;
+        letter-spacing: 1px;
+      }
+      i{
+        font-size: 17px;
+        color:  #C2C4CA;
+        font-weight: 600;
+      }
+    }
   }
 }
-</script>
+</style>
