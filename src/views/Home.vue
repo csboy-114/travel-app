cd52b69f93e95b8bc6718bd1eea110be1ad2ac79
csboy-114 <1060437021@qq.com>
2021-11-30 22:02:12 +0800

    重构页面跳转逻辑

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <component :is="currentComponent"></component>
+    <tabbar/>
+  </div>
+</template>
+
+<script>
+import tabbar from '../components/tabbar'
+
+export default {
+  components:{
+    tabbar
+  }
+}
+
+</script>
+<style lang='scss'>
+.home{
+  width: 100vw;
+  height: 100vh;
+}
+</style>
