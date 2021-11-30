1fc336608f4fe199eb8a17ff57302407aca716c7
csboy-114 <1060437021@qq.com>
2021-11-30 22:22:32 +0800

    删除路由废弃代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Index from '../views/Index.vue'
 import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
@@ -9,16 +8,6 @@ const routes = [
     path:'/',
     name:'Home',
     component: Home
-  },
-  {
-    path:'/destination',
-    name:'Destination',
-    component:() => import(/* webpackChunkName: "destination" */ '../views/Destination.vue')
-  },
-  {
-    path:'/personal',
-    name:'Personal',
-    component:()=>import(/* webpackChunkName: "personal" */ '../views/Personal.vue')
   }
 ]
 
