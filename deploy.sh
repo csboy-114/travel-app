d532fe66df7c810fbe2378dcd3ef5b02c67eaabf
csboy-114 <1060437021@qq.com>
2021-12-01 21:39:24 +0800

    sh修改

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:csboy-114/travel-app.git master:gh-pages
+git push -f git@github.com:csboy-114/travel-app.git feature:feature
 
 cd -
