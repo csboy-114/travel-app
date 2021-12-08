36e6d470db4b98c6dcda82cc6b3f132b354901a8
csboy-114 <1060437021@qq.com>
2021-12-08 21:17:35 +0800

    完成详情页中部偏上部分

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -8,6 +8,19 @@
       </span>
     </top-header>
     <div class="details-top"></div>
+    <div class="details-intros">
+      <p>
+        上海/广州直飞三亚5天亲子游（亲子/蜜月+豪华亲子酒店任意搭配+专车接送机+无限次畅游亚特水族馆和水世界+超值赠送5选一）
+      </p>
+      <div class="give">
+        <span>赠送接机</span>
+        <span>赠当地游项目</span>
+      </div>
+      <div class="counts">
+        <span>浏览 24004</span>
+        <span>已售 138份</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,6 +30,11 @@ export default {
   name:'Details',
   components:{
     TopHeader
+  },
+  data() {
+    return {
+      detailsInfo:{}
+    }
   },
   methods:{
     goBack(){
@@ -30,13 +48,7 @@ export default {
 .details{
   width: 100vw;
   height: 100vh;
-  &-top{
-    width: 100vw;
-    height: 230px;
-    background-image: url('../assets/images/图.png');
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
+  background-color: #fff;
   &-header{
     justify-content: space-between;
     align-items: center;
@@ -52,9 +64,55 @@ export default {
         font-size: 22px;
       }
       img{
-        width: 28px;
+        width: 30px;
         margin-left: 10px;
       }
+    }
+  }
+  &-top{
+    width: 100vw;
+    height: 230px;
+    background-image: url('../assets/images/图.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  &-intros{
+    width: 335px;
+    margin: 0 auto;
+    padding-top: 16px;
+    padding-bottom: 11px;
+    border-bottom: 1px solid #dadada;
+    p{
+      font-family: PingFangSC-Regular;
+      font-size: 14px;
+      color: #333333;
+      letter-spacing: 1px;
+      text-align: justify;
+      line-height: 24px;
+    }
+    .give{
+      display: flex;
+      margin-top: 8px;
+      span{
+        padding: 2px 12px;
+        font-family: PingFangSC-Regular;
+        font-size: 14px;
+        color: #5278A0;
+        line-height: 18px;
+        border: 1px solid #5278A0;
+        &:nth-child(1){
+          margin-right: 12px;
+        }
+      }
+    }
+    .counts{
+      display: flex;
+      justify-content: space-between;
+      font-family: PingFangSC-Regular;
+      margin-top: 12px;
+      font-size: 12px;
+      color: #999;
+      text-align: justify;
     }
   }
 }
